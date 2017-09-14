486d6353842d7b6f39a9fab56b06e59d875ffbe1
Kristoffer Teuber <krite@krite.se>
2017-09-14 21:48:25 +0200

    format

--- interpret.js
+++ interpret.js
@@ -1,11 +1,12 @@
 function inArray(item, arr) {
-	for(var a=0; a<arr.length; a++) {
+	for (var a = 0; a < arr.length; a++) {
 		if (arr[a] == item) {
 			return true;
 		}
 	}
 	return false;
 }
+
 //Real basic function to transform the program text in to an array of commands to execute
 function interpret(text) {
 	var commands = {
@@ -19,7 +20,7 @@ function interpret(text) {
 	var whitespace = [',', ' ', '\n'];
 
 	var execute = [];
-	for(var a=0; a<text.length; a++) {
+	for (var a = 0; a < text.length; a++) {
 		var char = text[a];
 		if (inArray(char, whitespace)) {
 			//Whitespace found
